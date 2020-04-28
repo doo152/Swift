31336503827d3fe9b0bb413c6ac5ac9df6e96791
Ken Zhang <zhangkun72@gmail.com>
2020-04-28 15:31:49 -0600

    Still got errors but can't find out.

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,10 +1,3 @@
+import UIKit
 
-//var str = "Hello, playground"
-//print(str.count)
-
-let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-var password = alphabet[Int.random(in: 0...35)]
-while(password.count < 8){
-    password += alphabet[Int.random(in: 0...35)]
-}
-print(password)
+var str = "Hello, playground"
